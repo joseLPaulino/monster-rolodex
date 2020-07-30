db4d4f1d8cf442de8e27d017cf758cccfe8ae6c2
josepaulino <jose.paulino@nokia-bell-labs.com>
2020-07-30 15:22:04 -0400

    Updates

---
+++ precache-manifest.f355ce86fd2b144393843d37be3e7b91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b561bca768b3af87d877a005444ca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd3cd4ad34938c1a518f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd3cd4ad34938c1a518f",
+    "url": "/static/js/main.74452e0e.chunk.js"
+  },
+  {
+    "revision": "a55fe9c0b312e2774e66",
+    "url": "/static/js/runtime-main.80c5e1e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
